c8c207229a25d5f7ed58dc4984b3a7ed6e5366c5
Ashwinth15 <ashwinthravi@gmail.com>
2025-05-15 10:18:53 +0530

    sanpe

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </a>hh
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn moree
       </p>
     </>
   )
